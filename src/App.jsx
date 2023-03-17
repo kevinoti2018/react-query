ab9deed55621f2fae28a04be98f1d755aba4907f
kevin otieno <kelvinotix@gmail.com>
2023-03-18 01:52:08 +0300

    react-query intermediate concepts

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,27 @@ function App() {
   return (
     <QueryClientProvider client={queryClient} >
     <Router>
-     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" >Home </Link>
-          </li>
-          <li>
-            <Link to="/super-heroes" > Traditional Super Heroes</Link>
-          </li>
-          <li>
-            <Link to="/rq-super-heroes" >RQ Super Heroes </Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-         <Route path='/' element={<Home/>} />
-         <Route path='/super-heroes' element={<SuperHeroes/>} />
-         <Route path='/rq-super-heroes' element={<RQSuperHeroes/>} />
-      </Routes>
-     </div>
-    </Router>
-     
+    <div>
+     <nav>
+       <ul>
+         <li>
+           <Link to="/" >Home </Link>
+         </li>
+         <li>
+           <Link to="/super-heroes" > Traditional Super Heroes</Link>
+         </li>
+         <li>
+           <Link to="/rq-super-heroes" >RQ Super Heroes </Link>
+         </li>
+       </ul>
+     </nav>
+     <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/super-heroes' element={<SuperHeroes/>} />
+        <Route path='/rq-super-heroes' element={<RQSuperHeroes/>} />
+     </Routes>
+    </div>
+   </Router>
     </QueryClientProvider>
   )
 }
--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 import {useQuery} from 'react-query'
 import axios from 'axios'
 const RQSuperHeroes = () => {
-  const fetchHeroes=()=>{
-    return axios.get('http://localhost:4000/superheroes1')
+  const onSuccess=()=>{
+    console.log("performs side effect after data fetcing")
   }
-  const {isLoading,data,isError,error}= useQuery('super-heroes',fetchHeroes)
-  if(isLoading){
+  const onError=()=>{
+    console.log("performs side effect after data fetcing")
+  }
+  const fetchHeroes=()=>{
+    return axios.get('http://localhost:4000/superheroes')
+  }
+  const {isLoading,data,isError,error,isFetching,refetch}= useQuery(
+    'super-heroes',
+    fetchHeroes,
+    {
+      //cacheTime:5000,// amount of time the fetched data will be available before refetching is required (deafult is 5 minutes)
+      //staleTime:0  //use the cached query results without having to refetch data in the background default(0)
+     // refetchOnmount:true , // data fetched evreytime the component mounts
+      //refetchOnWindoFocus: true
+      // enabled:false,
+      onSuccess,
+      onError
+    }
+    )
+  if(isLoading||isFetching){
     return <h1>Loading....</h1>
   }
   if(isError){
@@ -14,6 +32,8 @@ const RQSuperHeroes = () => {
   }
   return (  
     <div>
+    <h2>Rq super heroe page</h2>
+    <button onClick={refetch}>fetch heroes</button>
     {data?.data.map((hero)=>(
       <div key={hero.id}>
       <p>{hero.name}</p>
